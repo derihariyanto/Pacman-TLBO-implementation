07308cece787539f9a245adad52468ead2535f94
Deri Hariyanto <deri.hariyanto@students.amikom.ac.id>
2018-10-22 01:55:32 +0800

    add Pacman game without algorithm

---
+++ Assets/Scripts/Packman.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Packman : MonoBehaviour {
+
+    public float speed = 4.0f;
+    private Vector2 direct = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        CheckInput();
+        Move();
+        //movecloser();
+        UpdateOrientation();
+	}
+
+    void CheckInput() {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            direct = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            direct = Vector2.right;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            direct = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            direct = Vector2.down;
+        }
+    }
+
+    void Move() {
+        transform.localPosition += (Vector3)(direct * speed) * Time.deltaTime;
+        
+    }
+
+    void UpdateOrientation() {
+        if (direct == Vector2.left)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (direct == Vector2.right)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (direct == Vector2.up)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            transform.localRotation = Quaternion.Euler(0,0,90);
+        }
+        else if (direct == Vector2.down)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            transform.localRotation = Quaternion.Euler(0, 0, 270);
+        }
+
+        
+    }
+}
---
+++ Assets/Scripts/ghost.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public Transform[] waypoints;
+	int current_waypoints = 0; 
+
+	public float speed = 0.3f;
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (transform.position != waypoints[current_waypoints].position) {
+			Vector2 p = Vector2.MoveTowards(transform.position,
+											waypoints[current_waypoints].position,
+											speed);
+			GetComponent<Rigidbody2D>().MovePosition(p);
+		}
+		// Waypoint reached, select next one
+		else current_waypoints = (current_waypoints + 1) % waypoints.Length;
+
+		// Animation
+		Vector2 dir = waypoints[current_waypoints].position - transform.position;
+		GetComponent<Animator>().SetFloat("DirX", dir.x);
+		GetComponent<Animator>().SetFloat("DirY", dir.y);
+	}
+	void OnTriggerEnter2D(Collider2D co) {
+		if (co.name == "pacman")
+			Destroy(co.gameObject);
+	}
+}
